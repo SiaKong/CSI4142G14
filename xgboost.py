db4d65979d06a959236bb905a6f178cd6601706f
Coding0116 <39179456+SiaKong@users.noreply.github.com>
2023-04-11 17:08:57 -0400

    Create xgboost.py

---
+++ xgboost.py
@@ -0,0 +1,25 @@
+import pandas as pd 
+import numpy as np
+from sklearn.model_selection import train_test_split
+from xgboost import XGBRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from  sklearn.metrics import r2_score
+
+df = pd.read_csv('final.csv')
+
+X = df.iloc[:,:-1]
+y = df.iloc[:,-1]
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, random_state=100)
+
+xg_reg = XGBRegressor()
+
+xg_reg.fit(X_train, y_train)
+
+y_pred = xg_reg.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+rmse = np.sqrt(mse)
+mae = mean_absolute_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
