398a1fef974139ff0571f2d2092f65d5c6109bfb
Coding0116 <39179456+SiaKong@users.noreply.github.com>
2023-04-11 17:53:36 -0400

    Update xgboost.py

--- xgboost.py
+++ xgboost.py
@@ -23,3 +23,6 @@ mse = mean_squared_error(y_test, y_pred)
 rmse = np.sqrt(mse)
 mae = mean_absolute_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
+
+feature_names = X.columns
+feature_importance = pd.DataFrame(xg_reg.feature_importances_, index = feature_names).sort_values(0, ascending=False)
